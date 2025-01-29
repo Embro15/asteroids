910c69614725d5d1067e7142a08e6bc6c9204dcb
Embro15 <jesse.steinberg@mac.com>
2025-01-29 14:41:39 -0500

    CH2 L8: Groups

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     clock = pygame.time.Clock()
     dt = 0
+    
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     running = True
@@ -22,10 +27,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
         screen.fill("black")
-        player1.update(dt)
-        player1.draw(screen)
+        updatable.update(dt)
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000.0
 
 
