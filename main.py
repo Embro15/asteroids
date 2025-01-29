c1d10b668b767b0d7797344773265e0e39d053f9
Embro15 <jesse.steinberg@mac.com>
2025-01-29 13:50:47 -0500

    CH2 L3

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        dt = clock.tick(60) / 1000.0
 
 
 
