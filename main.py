5f939bb4c6cd44217c328120e143bdb89a9d6516
Embro15 <jesse.steinberg@mac.com>
2025-01-29 14:18:34 -0500

    Ch2 L6: moving around

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill("black")
+        player1.update(dt)
         player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
